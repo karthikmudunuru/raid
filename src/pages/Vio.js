d87d670178a75736e34518f8b2ff746c19998159
vmudunuru <vmudunuru@vmware.com>
2022-03-29 00:48:07 -0700

    VIO Setup

--- src/pages/Vio.js
+++ src/pages/Vio.js
@@ -1,43 +1,77 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useForm, FormProvider  } from "react-hook-form";
 import MyTextField from '../components/MyTextField';
-import MyDropdown from '../components/MyDropdown';
 import Password from '../components/Password';
 import { infraActions } from '../store/infra';
-import { myTextAreaStyle, submitStyle } from '../store/constants';
+import { submitStyle,mainStyle } from '../store/constants'
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
+import {vio_schema} from '../store/schemas';
 
 
+var vio_map;
+var dispatch_vio = {};
 
 
 
 
 const Vio= () =>{
     
-    const methods = useForm();
     const dispatch = useDispatch();
+    const methods = useForm({
+        resolver: yupResolver(vio_schema),
+        defaultValues:
+        {
+            vio_binary_path : '',
+            vio_domain_name : '',
+            vio_vm_name : '',
+            vio_mgmt_ip : '',
+            vio_start_mgmt_ip : '',
+            vio_end_mgmt_ip : '',
+            vio_gateway_addr : '',
+            vio_subnet_mask : '',
+            vio_username : '',
+            vio_user_password : '',
+         
+
+        }
+    
+    
+    });
+  
 
    
     
-    useEffect(()=>{
+ 
 
-        return () => {
-           
+    const onSubmit = data => {
+        
+       
+        vio_map = new Map(Object.entries(data));
+        
+       dispatch_vio["VIOOVAPath"] = vio_map.get("vio_binary_path");
+       dispatch_vio["VIODomainName"] = vio_map.get("vio_domain_name");
+       dispatch_vio["VIOVMName"] = vio_map.get("vio_vm_name");
+       dispatch_vio["VIOManagementIp"] = vio_map.get("vio_mgmt_ip");
+       dispatch_vio["VIOMgmtNetworkStartIP"] = vio_map.get("vio_start_mgmt_ip");
+       dispatch_vio["VIOMgmtNetworkEndIP"] = vio_map.get("vio_end_mgmt_ip");
+       dispatch_vio["VIOGateway"] = vio_map.get("vio_gateway_addr");
+       dispatch_vio["VIONetmask"] = vio_map.get("vio_subnet_mask");
+       dispatch_vio["VIOUserName"] = vio_map.get("vio_username");
+       dispatch_vio["VIOUserPassword"] = vio_map.get("vio_user_password");
+       
+        
+      dispatch(infraActions.setConfig(dispatch_vio))
 
-        }
-    },[]);
 
-
-    const onSubmit = data => console.log(data);
+    }
     
     
     return (
     
-        <div style={myTextAreaStyle}>
-            <FormProvider {...methods}>
-            <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <FormProvider {...methods}>
+        <form style={mainStyle} onSubmit={methods.handleSubmit(onSubmit)}>
+                <br/><br/>
             
                 <MyTextField label="VIO Ova Path " id="vio_binary_path"  />
                 <MyTextField label="VIO Domain name" id="vio_domain_name"  />
@@ -49,7 +83,7 @@ const Vio= () =>{
                 <MyTextField label="VIO Management Subnet Mask" id="vio_subnet_mask" />
                 <MyTextField label="VIO Node Username" id="vio_username" />
                 <Password label="VIO User Password" id="vio_user_password" />
-                <MyTextField label="VIO License Key" id="vio_license_key"  />
+              
                 
 
                 <input type="submit"  style={submitStyle} />
@@ -58,7 +92,7 @@ const Vio= () =>{
         
             </form>
             </FormProvider>
-        </div>
+      
     )
 };
 
